8735327c8b3653a2442cf3b7a09194f683daae6c
Cutie Deng <Dengzr2020.mail.sustech.edu.cn>
2022-05-12 16:59:18 +0800

    Give a simple selection problem.

--- demo.cpp
+++ demo.cpp
@@ -10,6 +10,14 @@ ostream &operator<<(ostream &o, MyClass &) {
 }
 
 int main() {
+    test1_question();
+}
+
+void test1_question() {
     MyClass ins; 
     std::cout << ins << std::endl; 
+    // Choose: A
+    // operator<<(std::cout, ins); 
+    // Choose: B 
+    // std::cout.operator<<(ins); 
 }
