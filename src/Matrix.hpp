ba039c5f74eb0f429a0c6361882e4d2415cc3cc3
Cutie Deng <Dengzr2020.mail.sustech.edu.cn>
2022-05-15 15:37:39 +0800

    Achieve the addition of two vectors!

---
+++ src/Matrix.hpp
@@ -0,0 +1,46 @@
+#include <cstdint> 
+#include <vector> 
+#include <type_traits> 
+#include <string> 
+#include <exception> 
+
+namespace Matrix {
+
+    template <typename A, typename B> 
+    struct FundamentalAdd {
+        using type = decltype(std::declval<A>() + std::declval<B>()); 
+    }; 
+
+    template <typename T> 
+    using DefaultAllocatorVector = std::vector<T, std::allocator<T>>; 
+
+    template <template <typename, typename> typename ResultTypeImpl = FundamentalAdd, 
+        template <typename...> typename Container = std::vector, 
+        typename LhsType, typename RhsType, 
+        template <typename... > typename LhsContainer, template <typename...> typename RhsContainer, 
+        typename... Useless1, nullptr_t = nullptr, typename... Useless2> 
+    auto add(LhsContainer<LhsType, Useless1...> const &lhs_matrix, RhsContainer<RhsType, Useless2...> const &rhs_matrix) -> Container<typename ResultTypeImpl<LhsType, RhsType>::type>  
+    {
+        auto result = Container<typename ResultTypeImpl<LhsType, RhsType>::type>{}; 
+        if (lhs_matrix.size() != rhs_matrix.size()) {
+            using namespace std::literals; 
+            throw std::runtime_error("Left Handside Matrix's size = "s + std::to_string(lhs_matrix.size()) + ", but right handside is " 
+                + std::to_string(rhs_matrix.size()) + ". "); 
+        }
+        size_t size = lhs_matrix.size(); 
+        if constexpr (requires {result.reserve(std::declval<size_t>()); }) {
+            result.reserve(size); 
+        }
+        for (size_t i = 0; i < size; ++i) {
+            result.push_back(lhs_matrix[i] + rhs_matrix[i]); 
+        }
+        return result; 
+    }
+
+    // template <template <typename...> typename A, template <typename ...> typename B, typename DataTypeA, typename DataTypeB, typename... AArgs, typename... BArgs, 
+    // template <typename> typename S = DefaultAllocatorVector > 
+    // auto add(A<DataTypeA, AArgs...>, B<DataTypeB, BArgs...>) {
+    //     std::cout << "add! \n"; 
+    // }
+
+}
---
+++ src/test.cpp
@@ -0,0 +1,19 @@
+#include <iostream> 
+#include "Matrix.hpp" 
+
+
+int main() {
+    std::vector<int> x {1, 2, 3, 4}; 
+    std::vector<float> y {2.5, 3.4, 4.3, .1}; 
+    auto &&v = Matrix::add(x, y); 
+    for (auto s: v) {
+        std::cout << s << " "; 
+    }
+    endl(std::cout); 
+    if constexpr (std::same_as<typename std::decay_t<decltype(v)>::value_type, float>) {
+        std::cout << "v is a vector with float elements! "; 
+    } else {
+        std::cout << "v is a vector with " << typeid(typename std::decay_t<decltype(v)>::value_type).name() << " type. ";
+    }
+    endl(std::cout);
+}
