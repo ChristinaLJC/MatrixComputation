833e08e65a052e094289f492e5f9175f7677ca73
Cutie Deng <Dengzr2020.mail.sustech.edu.cn>
2022-05-18 15:27:27 +0800

    Add some semicolons.

--- src/std.hpp
+++ src/std.hpp
@@ -29,9 +29,9 @@
 
 using std::nullptr_t; 
 
-using i16 = int16_t 
-using i32 = int32_t 
-using i64 = int64_t 
-using u16 = uint16_t 
-using u32 = uint32_t 
-using u64 = uint64_t 
+using i16 = int16_t; 
+using i32 = int32_t;  
+using i64 = int64_t; 
+using u16 = uint16_t;  
+using u32 = uint32_t;  
+using u64 = uint64_t;  
