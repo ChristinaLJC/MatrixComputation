bc2b9d588e0248baff6cc328de9f0cd539280e5b
ChristinaLJC <1315552992@qq.com>
2022-06-12 16:02:22 +0800

    update the inverse

--- src/algo/inverse.cpp
+++ src/algo/inverse.cpp
@@ -1,27 +1,32 @@
-template <typename T>
-T inverse(T const & origin) {
-    if (origin.row() != origin.col()) {
-        // throw exception
-    }
-
-    auto n = origin.row(); // the size of the matrix is n*n
+namespace matrix::algorithm {
     
-    T identity = identity_matrix(n); // make an identity matrix with origin's size
-
-    T elimination = gaussian_elimination(origin, identity);  // 高斯消元，包括向上回溯
-
-    T result = new_matrix(n, n); // use new_matrix to new a matrix
-    
-    for (int i = 0; i < n; ++i) {
-        auto pivot = elimination[i][i];
-        if (pivot != 0) {
-            for (int j = 0; j < n; ++j) {
-                result[i][j] = elimination[i][j+n] / pivot;
-            }
-        }
-        else {
+    template <typename ResultMatrix = typename type_traits::DefaultInverseMatrixType<OriginMatrix>::type, 
+            typename OriginMatrix>
+    ResultMatrix inverse(OriginMatrix const &self) {
+        if (self.row() != self.col()) {
             // throw exception
         }
+
+        auto n = self.row(); // the size of the matrix is n*n
+
+        OriginMatrix identity = identity_matrix(n); // make an identity matrix with origin's size
+
+        OriginMatrix elimination = gaussian_elimination(self, identity);  // 高斯消元，包括向上回溯
+
+        ResultMatrix result = new_matrix(n, n); // use new_matrix to new a matrix
+
+        for (int i = 0; i < n; ++i) {
+            auto pivot = elimination[i][i];
+            if (pivot != 0) {
+                for (int j = 0; j < n; ++j) {
+                    result[i][j] = elimination[i][j+n] / pivot;
+                }
+            }
+            else {
+                // throw exception
+            }
+        }
+        return result;
     }
-    return result;
 }
+
