4b3d683c54f185a251885a2543b17a1064d18abf
Cutie Deng <Dengzr2020.mail.sustech.edu.cn>
2022-06-13 16:07:17 +0800

    Add the inverse realization [[remake]].

--- src/lib/algorithm/inverse.hpp
+++ src/lib/algorithm/inverse.hpp
@@ -3,5 +3,29 @@
 #include "std.hpp" 
 
 namespace matrix::algorithm {
-    // template <typename T> 
+    template <typename ResultMatrix = Matrix, typename Matrix> 
+    ResultMatrix inverse (Matrix const &self) {
+        auto len = self.row(); 
+        if (len != self.col()) {
+            abort(); 
+        }
+
+        Matrix identity = Matrix::with_identity_size(len); 
+        Matrix elimination = gaussian_elimination(self, identity); 
+
+        ResultMatrix result = ResultMatrix::with_size(len); 
+
+        for (size_t i = 0; i < len; ++i) {
+            auto pivot = elimination[i][i]; 
+            if (pivot) {
+                for (size_t j = 0; j < len; ++j) {
+                    result[i][j] = elimination[i][j + len] / pivot; 
+                }
+            } else {
+                abort(); 
+            }
+        }
+
+        return result; 
+    }
 }
