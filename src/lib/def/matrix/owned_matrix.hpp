32da48dbc331a1ba1f960aa5f8da96d4232d475c
Cutie Deng <Dengzr2020.mail.sustech.edu.cn>
2022-06-13 15:53:36 +0800

    Finish the basic matrix realization.

--- src/lib/def/matrix/owned_matrix.hpp
+++ src/lib/def/matrix/owned_matrix.hpp
@@ -12,7 +12,7 @@ namespace matrix {
             using ContainerType = ValueContainer_<Args...>; 
             constexpr static bool is_fixed = false; 
             OwnedMatrix(size_t c, size_t r): 
-                ContainerType<ContainerType<ValueType>>(c, ContainerType<ValueType>(r)) {}  
+                ContainerType<ContainerType<ValueType>>(ContainerType<ValueType>(r), c) {}  
         public: 
             struct Visiter{
                 ContainerType<ValueType> &self; 
@@ -23,11 +23,31 @@ namespace matrix {
                     return self[index]; 
                 }
             }; 
-            // ContainerType<ValueType> &
+            struct ConstVisitor {
+                ContainerType<ValueType> const &self; 
+                ConstVisitor(OwnedMatrix const &self): self(self) {} 
+                ValueType const &operator[] (size_t index) {
+                    lassert (index < self.size()); 
+                    return self[index]; 
+                }
+            }; 
+
             Visiter operator[] (size_t index) {
-                lassert (index > 0); 
                 lassert (index < this->size()); 
                 return (*this)[index]; 
+            }
+            ConstVisitor operator[] (size_t index) const {
+                lassert (index < this->size()); 
+                return (*this)[index]; 
+            }
+            
+            size_t row() const noexcept {
+                return this->size(); 
+            }
+            size_t col() const {
+                lassert (this->size()); 
+                // return (*this)[0].self.size(); 
+                return this->ContainerType<ContainerType<ValueType>>::operator [](0).size(); 
             }
     }; 
 }
--- src/lib/test/matrix/constructor.cpp
+++ src/lib/test/matrix/constructor.cpp
@@ -1,5 +1,13 @@
 #include "def/matrix/owned_matrix.hpp"
 
-TEST_METHOD {
+Use matrix; 
 
+TEST_METHOD {
+    OwnedMatrix<int> simple(3, 3);  
+    bassert_eq (simple.row(), 3); 
+}
+
+TEST_METHOD {
+    OwnedMatrix<double> simple (4, 5); 
+    bassert_eq (simple.col(), 5); 
 }
