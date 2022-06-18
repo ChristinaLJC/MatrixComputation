5fdfb8ae19bbdc7ee054c7446924641987e9e739
Cutie Deng <Dengzr2020.mail.sustech.edu.cn>
2022-06-18 19:16:46 +0800

    Use requires to make a better support of stringizing method.

--- src/lib/def/mtype_traits.hpp
+++ src/lib/def/mtype_traits.hpp
@@ -1,23 +1,5 @@
 #pragma once 
 
-namespace matrix::algorithm {
-    template <typename NumberType> 
-    bool is_nearly_zero(NumberType const &v) {
-        constexpr auto abs_transform = [](auto const &v) -> decltype(v.abs()) {
-            return v.abs(); 
-        }; 
-        if constexpr (std::numeric_limits<std::decay_t<NumberType>>::is_integer) {
-            return !v; 
-        } else if (type_traits::is_impl(abs_transform)(v)) {
-            return is_nearly_same(abs_transform(v)); 
-            // return v <= std::numeric_limits<std::decay_t<NumberType>>::epsilon() && 
-            //     v >= - std::numeric_limits<std::decay_t<NumberType>>::epsilon(); 
-        } else {
-            return v >= 1e-6 && v <= -1e-6; 
-        }
-    }
-}
-
 namespace matrix::type_traits {
     template <typename F, typename... Args, typename = decltype(std::declval<F>()(std::declval<Args&&>()...))> 
     std::true_type IsImpl(nullptr_t ); 
@@ -46,6 +28,7 @@ namespace matrix::type_traits {
         auto constexpr fetch_name_trait = [] (auto &&a) constexpr -> std::enable_if_t<std::is_same_v<decltype(a.name()), char const *>, std::string> {
             return std::string(a.name()); 
         }; 
+        // auto constexpr complex_trait = [] (auto &&a) constexpr -> 
         
         if constexpr (decltype(type_traits::is_impl(to_string_op_trait)(v))::value) {
             return to_string_op_trait(v); 
@@ -57,6 +40,12 @@ namespace matrix::type_traits {
             return std_to_string_output(v); 
         } else if constexpr (decltype(type_traits::is_impl(fetch_name_trait)(v))::value) {
             return fetch_name_trait(v); 
+        } else if constexpr (requires {
+            // v.real; 
+            // v.imag; 
+            { stringizing(v.real()) + " + " + stringizing(v.imag()) + "i" } -> std::same_as<std::string>; 
+        }) {
+            return stringizing(v.real()) + " + " + stringizing(v.imag()) + "i"; 
         }
         return "[Not Realize String Trait]"; 
     }
@@ -91,14 +80,40 @@ namespace matrix::type_traits {
         return f; 
     }
 
+    // template <typename Lhs, typename Rhs> 
+    // bool is_nearly_same (Lhs const &, Rhs const &); 
+
+}
+
+namespace matrix::algorithm {
+    template <typename NumberType> 
+    bool is_nearly_zero(NumberType const &v) {
+        constexpr auto abs_transform = [](auto const &v) -> std::enable_if_t<std::is_same_v<void, std::void_t<decltype(abs(v))>>, typename std::decay_t<decltype(v)>::value_type> {
+            // return std::abs(v); 
+            return abs(v); 
+        }; 
+        if constexpr (std::numeric_limits<std::decay_t<NumberType>>::is_integer) {
+            return !v; 
+        } else if constexpr (decltype(type_traits::is_impl(abs_transform)(v))::value) {
+            // std::cout << __FILE__ << ":" << __LINE__ << '\n'; 
+            // static_assert (std::is_same_v<decltype(abs_transform(v)), typename NumberType::value_type>); 
+            return is_nearly_zero(abs_transform(v)); 
+            // return v <= std::numeric_limits<std::decay_t<NumberType>>::epsilon() && 
+            //     v >= - std::numeric_limits<std::decay_t<NumberType>>::epsilon(); 
+        } else {
+            return v >= 1e-6 && v <= -1e-6; 
+        }
+    }
+}
+
+namespace matrix::type_traits {
     template <typename Lhs, typename Rhs> 
     bool is_nearly_same(Lhs const &lhs, Rhs const &rhs) {
-        if constexpr (!std::numeric_limits<Lhs>::is_integer && 
+        if constexpr (!std::numeric_limits<Lhs>::is_integer || 
             !std::numeric_limits<Rhs>::is_integer) {
-            return algorithm::is_nearly_zero(lhs - rhs); 
+            return algorithm::is_nearly_zero(lhs - (Lhs)rhs); 
         } else {
             return lhs == rhs; 
         }
     }
-
 }
--- src/lib/test/matrix/linear_owned_matrix/constructor.cpp
+++ src/lib/test/matrix/linear_owned_matrix/constructor.cpp
@@ -79,7 +79,7 @@ TEST_METHOD {
 }
 
 TEST_METHOD {
-    auto matrix = LinearOwnedMatrix<std::complex<int>>::with_identity_size(3); 
+    auto matrix = LinearOwnedMatrix<std::complex<float>>::with_identity_size(3); 
     bassert_eq (matrix[0][0], 1); 
     using namespace std::literals; 
     matrix[0][0] = 1i; 
