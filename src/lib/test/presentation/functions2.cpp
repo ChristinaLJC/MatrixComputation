b29bdca39af55a10960d476c08df936773a0ee02
DG <1611245010@qq.com>
2022-06-19 14:33:23 +0800

    add some tests

--- src/lib/test/presentation/functions2.cpp
+++ src/lib/test/presentation/functions2.cpp
@@ -50,3 +50,78 @@ TEST_METHOD {
     bassert_eq(z[1][2], 0);
     bassert_eq(z[2][2], 1);
 }
+
+TEST_METHOD{
+    LinearOwnedMatrix<int> x(5000,5000);
+    for (int i = 0; i < 5000; ++i){
+        for (int j = 0; j < 5000; ++j){
+            x[i][j] = i+j;
+        }
+    }
+    bassert_eq(x[1000][2000], 3000);
+    bassert_eq(x[3456][3456], 3456+3456);
+}
+
+TEST_METHOD{
+    LinearOwnedMatrix<int> x(3,3);
+    x[0][0] = 1;
+    x[0][1] = 2;
+    x[0][2] = 3;
+    x[1][0] = 4;
+    x[1][1] = 5;
+    x[1][2] = 6;
+    x[2][0] = 7;
+    x[2][1] = 8;
+    x[2][2] = 9;
+    bassert_eq(x.max(), 9);
+    bassert_eq(x.min(), 1);
+    bassert_eq(x.sum(), 45);
+    bassert_eq(x.avg(), 5);
+    bassert_eq(x.max(0,1,0,2), 6);
+    bassert_eq(x.min(0,1,0,2), 1);
+    bassert_eq(x.sum(0,1,0,2), 21);
+    bassert_eq(x.avg(0,1,0,2), 3);
+}
+
+TEST_METHOD{
+    LinearOwnedMatrix<int> x(3,3);
+    x[0][0] = 1;
+    x[0][1] = 2;
+    x[0][2] = 3;
+    x[1][0] = 4;
+    x[1][1] = 5;
+    x[1][2] = 6;
+    x[2][0] = 7;
+    x[2][1] = 8;
+    x[2][2] = 9;
+    auto y = slice(0,1,0,2);
+    bassert_eq(y[0][0], 1);
+    bassert_eq(y[0][1], 2);
+    bassert_eq(y[0][2], 3);
+    bassert_eq(y[1][0], 4);
+    bassert_eq(y[1][1], 5);
+    bassert_eq(y[1][2], 6);
+}
+
+TEST_METHOD{
+    LinearOwnedMatrix<int> x(3,3);
+    x[0][0] = 1;
+    x[0][1] = 2;
+    x[0][2] = 3;
+    x[1][0] = 4;
+    x[1][1] = 5;
+    x[1][2] = 6;
+    x[2][0] = 7;
+    x[2][1] = 8;
+    x[2][2] = 9;
+    auto y = reshape(1,9);
+    bassert_eq(y[0][0], 1);
+    bassert_eq(y[0][1], 2);
+    bassert_eq(y[0][2], 3);
+    bassert_eq(y[0][3], 4);
+    bassert_eq(y[0][4], 5);
+    bassert_eq(y[0][5], 6);
+    bassert_eq(y[0][6], 7);
+    bassert_eq(y[0][7], 8);
+    bassert_eq(y[0][8], 9);
+}
